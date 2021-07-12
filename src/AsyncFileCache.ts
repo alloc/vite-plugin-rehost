12c7f678173580a1351e89e6b067777bc1239919
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-07-12 17:40:32 -0400

    refactor: add `AsyncFileCache` class
    - Files are now lazy-loaded (usually from `resolveBuiltUrl`)
    - The manual writer now waits for nested assets to finish loading
    - Asset urls in CSS files are now properly rewritten in manual-write mode

---
+++ src/AsyncFileCache.ts
@@ -0,0 +1,155 @@
+import cheerio from 'cheerio'
+import MagicString from 'magic-string'
+import fetch, { Response } from 'node-fetch'
+import { URL } from 'url'
+import urlRegex from 'url-regex'
+import { relative } from '@cush/relative'
+
+type Element = ReturnType<typeof cheerio>
+type FileCache = { [id: string]: FilePromise }
+type FilePromise = Promise<string | Buffer>
+type FileLoader = () => FilePromise
+
+const debug = require('debug')('vite-rehost')
+
+export class AsyncFileCache {
+  private files: FileCache = {}
+  private filesPromise: Promise<any> = Promise.resolve()
+  private loaders: { [id: string]: FileLoader } = {}
+  private requests: { [url: string]: Promise<Response> } = {}
+
+  constructor(private toPublicUrl: (url: string) => string | Promise<string>) {}
+
+  has(id: string) {
+    return id in this.files || id in this.loaders
+  }
+  get(id: string) {
+    let file = this.files[id]
+    if (!file) {
+      const loadFile = this.loaders[id]
+      if (loadFile) {
+        this.set(id, (file = loadFile()))
+        delete this.loaders[id]
+      }
+    }
+    return file
+  }
+  set(id: string, promise: Promise<string | Buffer>) {
+    this.files[id] = promise
+    const prevPromise = this.filesPromise
+    this.filesPromise = promise.then(() => prevPromise)
+  }
+  async entries() {
+    let promise: Promise<any> | undefined
+    while (promise !== (promise = this.filesPromise)) {
+      await promise
+    }
+    return Promise.all(
+      Object.entries(this.files).map(file =>
+        file[1].then(source => [file[0], source] as const)
+      )
+    )
+  }
+  fetchStyles(cssRef: Element) {
+    const cssUrl = cssRef.attr('href')
+    if (cssUrl && isExternalUrl(cssUrl)) {
+      const file = toFilePath(cssUrl)
+      cssRef.attr('href', file)
+
+      if (!this.files[file])
+        this.loaders[file] ??= () =>
+          this.fetchText(cssUrl).then(cssText =>
+            replaceCssUrls(cssText, cssUrl, url => {
+              return this.fetchAsset(url)
+            })
+          )
+    }
+  }
+  fetchScript(scriptRef: Element) {
+    const scriptUrl = scriptRef.attr('src')
+    if (scriptUrl && isExternalUrl(scriptUrl)) {
+      const file = toFilePath(scriptUrl)
+      scriptRef.attr('src', file)
+
+      if (!this.files[file]) {
+        this.loaders[file] ??= () => this.fetchText(scriptUrl)
+      }
+    }
+  }
+  fetchAsset(assetUrl: string) {
+    const file = toFilePath(assetUrl)
+    if (!this.files[file]) {
+      this.loaders[file] ??= () => this.fetchBuffer(assetUrl)
+    }
+    return this.toPublicUrl(file)
+  }
+  fetchText(url: string) {
+    return this.fetch(url).then(res => res.text())
+  }
+  fetchBuffer(url: string) {
+    return this.fetch(url).then(res => res.buffer())
+  }
+  private fetch(url: string) {
+    return this.requests[url] || (this.requests[url] = fetch(url))
+  }
+}
+
+function toFilePath(url: string) {
+  let file = '/'
+
+  const { host, pathname, searchParams } = new URL(url)
+  if (host == 'www.googletagmanager.com') {
+    file += `${host}/gtag.js`
+  } else if (host == 'fonts.googleapis.com') {
+    const [family] = searchParams.get('family')!.split(':')
+    file += `${host}/${family}.css`
+  } else {
+    file += host + decodeURIComponent(pathname)
+  }
+
+  return file
+}
+
+function isExternalUrl(url: string) {
+  return urlRegex().test(url)
+}
+
+async function replaceCssUrls(
+  text: string,
+  parentUrl: string,
+  replacer: (url: string) => string | Promise<string>
+) {
+  const editor = new MagicString(text)
+  const loading: Promise<void>[] = []
+  const cssUrlRE = /url\(\s*('[^']+'|"[^"]+"|[^'")]+)\s*\)/g
+  for (;;) {
+    const match = cssUrlRE.exec(text)
+    if (!match) {
+      await Promise.all(loading)
+      return editor.toString()
+    }
+    let url = match[1]
+    if (/^['"]/.test(url)) {
+      url = url.slice(1, -1)
+    }
+    const prevUrl = url
+    if (/^\.\.?\//.test(url)) {
+      url = relative(parentUrl, url) || url
+      debug(`resolve "${prevUrl}" to "${url}"`)
+    } else if (!isExternalUrl(url)) {
+      url = parentUrl.slice(0, parentUrl.lastIndexOf('/') + 1) + url
+      debug(`resolve "${prevUrl}" to "${url}"`)
+    }
+    if (isExternalUrl(url))
+      loading.push(
+        Promise.resolve(replacer(url)).then(url => {
+          debug(`save as "${url}"`)
+          editor.overwrite(
+            match.index + 4,
+            match.index + match[0].length - 1,
+            JSON.stringify(url)
+          )
+        })
+      )
+  }
+}
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,222 +1,110 @@
-import type { Plugin } from 'vite'
-import urlRegex from 'url-regex'
-import MagicString from 'magic-string'
-import { relative } from '@cush/relative'
+import { Plugin, normalizePath } from 'vite'
 import cheerio from 'cheerio'
-import fetch from 'node-fetch'
 import path from 'path'
 import fs from 'fs'
-import { URL } from 'url'
 import { createHash } from 'crypto'
-
-const debug = require('debug')('vite-rehost')
-
-type Element = ReturnType<typeof cheerio>
-type FileCache = { [file: string]: string | Buffer }
+import { AsyncFileCache } from './AsyncFileCache'
 
 export default (): Plugin => {
-  const files: FileCache = {}
+  let base: string
+  let outDir: string
+  let assetsDir: string
+  let useEmitFile = true
+
   const emitCache = new Map<string, string>()
+  const files = new AsyncFileCache(async id => {
+    if (useEmitFile) {
+      return id
+    }
+    const source = await files.get(id)
+    const fileName = getFileName(id.slice(1), getAssetHash(source), assetsDir)
+    emitCache.set(id, path.resolve(outDir, fileName))
+    return base + fileName
+  })
 
   return {
     name: 'vite:rehost',
     apply: 'build',
     enforce: 'pre',
-    configResolved({ root, base, build: { assetsDir, outDir } }) {
-      let useEmitFile = true
-
-      this.resolveBuiltUrl = function (id) {
-        const source = files[id]
-        if (source == null) {
-          return null
-        }
-        let assetId = emitCache.get(id)
-        if (!assetId) {
-          const fileName = getFileName(
-            id.slice(1),
-            getAssetHash(source),
-            assetsDir
-          )
-          if (!useEmitFile) {
-            emitCache.set(id, path.resolve(root, outDir, fileName))
-            return base + fileName
-          }
-          assetId = this.emitFile({
-            type: 'asset',
-            fileName,
-            source,
-          })
-          // Vite replaces __VITE_ASSET__ imports in its default plugins
-          emitCache.set(id, (assetId = `__VITE_ASSET__${assetId}__`))
-        }
-        return assetId
-      }
-
-      this.writeBundle = () => {
-        // No files have been emitted yet? That means we need
-        // to write files manually in the `closeBundle` phase.
-        if (!emitCache.size) {
-          useEmitFile = false
-        }
-      }
-
-      this.closeBundle = async function () {
-        if (!useEmitFile)
-          for (const [id, source] of Object.entries(files)) {
-            const filePath = emitCache.get(id)
-            if (filePath) {
-              fs.mkdirSync(path.dirname(filePath), { recursive: true })
-              fs.writeFileSync(filePath, source)
-            }
-          }
-
-        // Clear the emit cache so `emitFile` is called
-        // again on rebuilds (in watch mode).
-        emitCache.clear()
-      }
+    configResolved(config) {
+      base = config.base
+      outDir = path.resolve(config.root, config.build.outDir)
+      assetsDir = normalizePath(config.build.assetsDir)
     },
     resolveId(id) {
-      if (files[id]) {
+      if (files.has(id)) {
         return id
       }
     },
     // Self-hosted files may be bundled.
-    load(id) {
-      return files[id]?.toString()
+    async load(id) {
+      const source = await files.get(id)
+      return source?.toString()
     },
     transformIndexHtml: {
       enforce: 'pre',
       async transform(html) {
         const $ = cheerio.load(html)
-        const loading: Promise<void>[] = []
 
         $('link[rel="stylesheet"]').each((_i, el) => {
-          const url = $(el).attr('href')
-          if (url && isExternalUrl(url)) {
-            loading.push(fetchStyles(url, $(el), files))
-          }
+          files.fetchStyles($(el))
         })
-
         $('script[src]').each((_i, el) => {
-          const url = $(el).attr('src')
-          if (url && isExternalUrl(url)) {
-            loading.push(fetchScript(url, $(el), files))
-          }
+          files.fetchScript($(el))
         })
 
-        await Promise.all(loading)
         return $.html()
       },
     },
+    async resolveBuiltUrl(id) {
+      const source = await files.get(id)
+      if (source == null) {
+        return null
+      }
+      let assetId = emitCache.get(id)
+      if (!assetId) {
+        const fileName = getFileName(
+          id.slice(1),
+          getAssetHash(source),
+          assetsDir
+        )
+        if (!useEmitFile) {
+          emitCache.set(id, path.resolve(outDir, fileName))
+          return base + fileName
+        }
+        assetId = this.emitFile({
+          type: 'asset',
+          fileName,
+          source,
+        })
+        // Vite replaces __VITE_ASSET__ imports in its default plugins
+        emitCache.set(id, (assetId = `__VITE_ASSET__${assetId}__`))
+      }
+      return assetId
+    },
+    writeBundle: () => {
+      // No files have been emitted yet? That means we need
+      // to write files manually in the `closeBundle` phase.
+      if (!emitCache.size) {
+        useEmitFile = false
+      }
+    },
+    async closeBundle() {
+      if (!useEmitFile)
+        for (const [id, source] of await files.entries()) {
+          const filePath = emitCache.get(id)
+          if (filePath) {
+            fs.mkdirSync(path.dirname(filePath), { recursive: true })
+            fs.writeFileSync(filePath, source)
+          }
+        }
+
+      // Clear the emit cache so `emitFile` is called
+      // again on rebuilds (in watch mode).
+      emitCache.clear()
+      useEmitFile = true
+    },
   }
-}
-
-async function fetchStyles(url: string, el: Element, files: FileCache) {
-  const file = toFilePath(url)
-  el.attr('href', file)
-
-  if (files[file] == null) {
-    files[file] = ''
-
-    const loading: Promise<void>[] = []
-
-    const parentUrl = url
-    const text = await fetchText(url)
-    files[file] = replaceCssUrls(text, parentUrl, url => {
-      loading.push(fetchAsset(url, files))
-      return toFilePath(url)
-    })
-
-    await Promise.all(loading)
-  }
-}
-
-async function fetchScript(url: string, el: Element, files: FileCache) {
-  const file = toFilePath(url)
-  el.attr('src', file)
-
-  if (files[file] == null) {
-    files[file] = ''
-    files[file] = await fetchText(url)
-  }
-}
-
-function isExternalUrl(url: string) {
-  return urlRegex().test(url)
-}
-
-const textCache: { [url: string]: Promise<string> } = {}
-const bufferCache: { [url: string]: Promise<Buffer> } = {}
-
-function fetchText(url: string) {
-  return textCache[url] || (textCache[url] = fetch(url).then(res => res.text()))
-}
-
-function fetchBuffer(url: string) {
-  return (
-    bufferCache[url] ||
-    (bufferCache[url] = fetch(url).then(res => res.buffer()))
-  )
-}
-
-async function fetchAsset(url: string, files: FileCache) {
-  const file = toFilePath(url)
-  if (files[file] == null) {
-    files[file] = ''
-    files[file] = await fetchBuffer(url)
-  }
-}
-
-function replaceCssUrls(
-  text: string,
-  parentUrl: string,
-  replacer: (url: string) => string
-) {
-  const editor = new MagicString(text)
-  const cssUrlRE = /url\(\s*('[^']+'|"[^"]+"|[^'")]+)\s*\)/g
-  for (;;) {
-    const match = cssUrlRE.exec(text)
-    if (!match) {
-      return editor.toString()
-    }
-    let url = match[1]
-    if (/^['"]/.test(url)) {
-      url = url.slice(1, -1)
-    }
-    const prevUrl = url
-    if (/^\.\.?\//.test(url)) {
-      url = relative(parentUrl, url) || url
-      debug(`resolve "${prevUrl}" to "${url}"`)
-    } else if (!isExternalUrl(url)) {
-      url = parentUrl.slice(0, parentUrl.lastIndexOf('/') + 1) + url
-      debug(`resolve "${prevUrl}" to "${url}"`)
-    }
-    if (isExternalUrl(url)) {
-      url = replacer(url)
-      debug(`save as "${url}"`)
-      editor.overwrite(
-        match.index + 4,
-        match.index + match[0].length - 1,
-        JSON.stringify(url)
-      )
-    }
-  }
-}
-
-function toFilePath(url: string) {
-  let file = '/'
-
-  const { host, pathname, searchParams } = new URL(url)
-  if (host == 'www.googletagmanager.com') {
-    file += `${host}/gtag.js`
-  } else if (host == 'fonts.googleapis.com') {
-    const [family] = searchParams.get('family')!.split(':')
-    file += `${host}/${family}.css`
-  } else {
-    file += host + decodeURIComponent(pathname)
-  }
-
-  return file
 }
 
 function getFileName(file: string, contentHash: string, assetsDir: string) {
